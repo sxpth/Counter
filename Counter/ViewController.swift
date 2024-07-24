b6b73f94418800a93f699db83dce2a9cac6dd194
sxpth <yuruse@mail.ru>
2024-07-24 20:35:50 +0300

    Задача выполнена

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateCounterLabel()
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        counter += 1
+        updateCounterLabel()
+    }
+    
+    private func updateCounterLabel() {
+        counterLabel.text = "Изменение счётчика: \(counter)"
+    }
+    
 }
 
