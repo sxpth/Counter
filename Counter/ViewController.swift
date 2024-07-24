a5c29fc4699fe2b6c742e6943bbab2a7e0e3c330
sxpth <yuruse@mail.ru>
2024-07-24 20:32:12 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Дмитрий Ковач on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
